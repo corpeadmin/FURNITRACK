0f153f7c7ba02812eaaa5cf8ae301bf7d65c0d35
Alexander Sapo <senpaizengki@gmail.com>
2026-04-22 14:51:57 +0800

    update

--- obj/Debug/net8.0/FURNITRACK.AssemblyInfo.cs
+++ obj/Debug/net8.0/FURNITRACK.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FURNITRACK")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d11a1b0bfec80cc323b16102d51cd1ef38dc0b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("FURNITRACK")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FURNITRACK")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
